f364fa0cbcac6b276de74d6642c61b6e001c073f
Gabriel Serengheu <Gabriel.Serengheu@msg.group>
2023-07-20 09:17:09 +0300

    fixed backend, added CrossOrigins

--- BE_spring/src/main/java/com/example/be_spring/Controller.java
+++ BE_spring/src/main/java/com/example/be_spring/Controller.java
@@ -1,5 +1,6 @@
 package com.example.be_spring;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    @CrossOrigin(origins = "https://localhost:4200")
     @GetMapping("/hello")
     public String getHelloWorld(@RequestParam(value="myName", defaultValue="World") String name){
         return String.format("Hello %s!", name);
